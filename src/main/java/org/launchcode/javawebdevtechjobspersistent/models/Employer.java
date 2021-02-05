3d58b14ac9f551a355e39614408aee469bd86146
Dhwani Patel <dhwanipatel2125@gmail.com>
2021-02-05 13:34:57 -0600

    Part 1 & 2 Completed.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,33 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotBlank(message ="Location is required")
+    @Size(min = 3, max= 255, message= "Location must be between 3 and 255 characters")
+    private String location;
+
+    //Constructors
+    public Employer() {
+
+    }
+    public Employer(String location) {
+        super();
+        this.location = location;
+    }
+
+    //getters and setters
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,30 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+
+@Entity
 public class Skill extends AbstractEntity {
+    @NotBlank(message ="Description is required")
+    private String description;
+
+// Constructors
+    public Skill() {
+    }
+
+    public Skill(String description) {
+        super();
+        this.description = description;
+    }
+
+    //getter and setters
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
 }
